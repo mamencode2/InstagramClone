b099e23f906e15f789263cdde002b1c8c3c1c27a
mamencode2 <mamencode2@gmail.com>
2022-02-16 17:19:10 +0000

    cd

--- admin/src/config/config.js
+++ admin/src/config/config.js
@@ -1,11 +1,10 @@
 export var firebaseConfig = {
-  apiKey: "****",
-  authDomain: "****",
-  databaseURL: "****",
-  projectId: "****",
-  storageBucket: "****",
-  messagingSenderId: "****",
-  appId: "****",
-  measurementId: "****"
+  apiKey: "AIzaSyA1t2roqz1jZrqGuRz0v_NuakppQiL0iDo",
+  authDomain: "instagram-38fb0.firebaseapp.com",
+  projectId: "instagram-38fb0",
+  storageBucket: "instagram-38fb0.appspot.com",
+  messagingSenderId: "1016811933098",
+  appId: "1:1016811933098:web:d2cc64898057a497acefd3",
+  measurementId: "G-9LW4ET9Y9X"
 
 };
